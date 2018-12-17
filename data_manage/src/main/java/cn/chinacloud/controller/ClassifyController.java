8618166e6e657b7decca267ca2fd3e838f065467
liuweiping <liuweiping@chinacloud.com.cn>
2018-12-17 17:18:34 +0800

    定义资源分类接口

---
+++ data_manage/src/main/java/cn/chinacloud/controller/ClassifyController.java
@@ -0,0 +1,7 @@
+package cn.chinacloud.controller;
+
+/**
+ * Created by Administrator on 2018/12/17.
+ */
+public class ClassifyController {
+}
---
+++ data_manage/src/main/java/cn/chinacloud/service/ClassifyService.java
@@ -0,0 +1,7 @@
+package cn.chinacloud.service;
+
+/**
+ * Created by Administrator on 2018/12/17.
+ */
+public class ClassifyService {
+}
